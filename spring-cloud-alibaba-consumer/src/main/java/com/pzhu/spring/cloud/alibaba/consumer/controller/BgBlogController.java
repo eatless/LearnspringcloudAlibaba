a03f995fc562b9d0e496a1783cb2437a65bebcbb
金海洋 <jinhaiyang@delonix.group>
2023-04-25 21:41:32 +0800

    下载url

--- spring-cloud-alibaba-consumer/src/main/java/com/pzhu/spring/cloud/alibaba/consumer/controller/BgBlogController.java
+++ spring-cloud-alibaba-consumer/src/main/java/com/pzhu/spring/cloud/alibaba/consumer/controller/BgBlogController.java
@@ -2,22 +2,24 @@ package com.pzhu.spring.cloud.alibaba.consumer.controller;
 
 import com.pzhu.spring.cloud.alibaba.common.domain.BgBlog;
 
-import com.pzhu.spring.cloud.alibaba.consumer.req.UploadFileReqDTO;
 import com.pzhu.spring.cloud.alibaba.consumer.service.BgBlogServiceFeign;
 import com.pzhu.spring.cloud.alibaba.consumer.service.ExcelService;
 import com.pzhu.spring.cloud.alibaba.consumer.service.ThreadUtilService;
-import com.pzhu.spring.cloud.alibaba.consumer.service.UpFileService;
-import io.swagger.annotations.ApiOperation;
+import com.pzhu.spring.cloud.alibaba.consumer.service.FileService;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Objects;
-import java.util.Random;
-import java.util.concurrent.CountDownLatch;
+import java.io.OutputStream;
+import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,7 +48,7 @@ public class BgBlogController {
     private ThreadUtilService threadUtilService;
 
     @Autowired
-    private UpFileService upFileService;
+    private FileService fileService;
     /**
      * 查询列表
      *
@@ -99,27 +101,36 @@ public class BgBlogController {
     }
 
 
-    /**
-     * 传入对象的方式
-     * @param dto
-     */
-    @PostMapping(value = "/fileUpload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public void upload(UploadFileReqDTO dto) {
-
-        MultipartFile file = dto.getFile();
-        String fileName = file.getOriginalFilename();
-        System.out.println(fileName);
-
-    }
 
 
-    @PostMapping("/uplode3")
+    @PostMapping("/fileUpload")
     @ResponseBody
     public String getUplode(@RequestPart("one") MultipartFile mf ) throws IOException {
 
         log.info("单文件上传信息为:" + mf.getOriginalFilename());
-        upFileService.updatefile(mf);
+        fileService.updatefile(mf);
 
         return "成功";
+    }
+
+    @GetMapping("/downLoad")
+    //http://localhost:8083/downLoad?filename=1.pdf
+    @ResponseBody
+    public void download(HttpServletResponse response,@RequestParam("filename") String fileName) throws IOException {
+
+        OutputStream outputStream = null;
+        try {
+            outputStream = response.getOutputStream();
+            File file = new File("/Users/jinhaiyang/Desktop/upfileAndDownFile/" + fileName);
+            if(!file.exists() || !file.isFile()){
+                outputStream.write("no this file".getBytes());
+                return;
+            }
+            FileInputStream fileInputStream =new FileInputStream(file);
+            IOUtils.copy(fileInputStream,outputStream);
+        } catch (IOException e) {
+//            logger.error("获取文件,系统异常， 路径：{}",path);
+            e.printStackTrace();
+        }
     }
 }
--- spring-cloud-alibaba-consumer/src/main/java/com/pzhu/spring/cloud/alibaba/consumer/service/FileService.java
+++ spring-cloud-alibaba-consumer/src/main/java/com/pzhu/spring/cloud/alibaba/consumer/service/FileService.java
@@ -1,11 +1,15 @@
 package com.pzhu.spring.cloud.alibaba.consumer.service;
 
+import cn.hutool.extra.servlet.ServletUtil;
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.write.builder.ExcelWriterBuilder;
 import com.pzhu.spring.cloud.alibaba.consumer.excel.Listener.ExcelListener;
 import com.pzhu.spring.cloud.alibaba.consumer.excel.WriteData.DemoData;
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -13,30 +17,47 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @Slf4j
-public class UpFileService {
+public class FileService {
 
     public void updatefile(MultipartFile file) throws IOException {
         // 文件大小不能超过5M
-        if(Objects.isNull(file) || file.getSize() == 0){
+        if (Objects.isNull(file) || file.getSize() == 0) {
             log.info("附件或原始简历不能为空");
         }
 
-        if(Objects.nonNull(file) && file.getSize() > (5 * 1024 * 1024)){
+        if (Objects.nonNull(file) && file.getSize() > (5 * 1024 * 1024)) {
             log.info("附件或原始简历不能为空或大于5M;");
         }
         /*将文件上传到指定文件夹*/
         if (!file.isEmpty()) {
             String fileName = file.getOriginalFilename();
-            log.info("文件名字为"+fileName);
+            log.info("文件名字为" + fileName);
+            System.out.println("文件名字为" + fileName);
             //文件上传
-            file.transferTo(new File("E:\\" + fileName));
+            file.transferTo(new File("/Users/jinhaiyang/Desktop/upfileAndDownFile/" + fileName));
         }
     }
 
 
+    public Resource download(String fileName) {
+        Path file = Paths.get("/Users/jinhaiyang/Desktop/upfileAndDownFile/" + fileName);
+        //下载文件
+        try {
+            Resource resource = new UrlResource(file.toUri());
+            if (resource.exists() && resource.isReadable()) {
+                return resource;
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
 }
